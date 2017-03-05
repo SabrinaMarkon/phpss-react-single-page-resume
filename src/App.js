2a0c11630ec2c5c782eba4e95df53dae99034b96
Sabrina Markon <phpsitescripts@outlook.com>
2017-03-05 10:05:23 +0000

    -json source file created. -ajax code added to App.js to get json data. -jquery installed and imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
@@ -9,7 +10,36 @@ import Resume from './Components/Resume';
 import Testimonials from './Components/Testimonials';
 
 class App extends Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+     foo: 'bar',
+     resumeData: {}
+   }
+  }
+  getResumeData() {
+    $.ajax({
+      url: 'resumeData.json',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        /* if all goes well: */
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        /* if anything goes wrong: */
+        console.og(err);
+        alert(err);
+      }
+    });
+  }
+  /* typically ajax request goes in a lifecycle method called componentDidMount */
+  componentDidMount() {
+    /* call method above - need to install jquery for ajax request. (yes I know it doesn't HAVE to be with jquery.) */
+    this.getResumeData();
+  }
   render() {
+    console.log(this.state.resumeData);
     return (
       <div className="App">
         <Header />
